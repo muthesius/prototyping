1cf8fbbcab234c04aa77a2f5bd9eb3c8c72c9a59
jens <jens@lea.io>
2016-01-14 16:04:28 +0100

    Enable simple power supply

--- 3D-Printing/ceramic-printer/firmware/Marlin-Configurations/Configuration.h
+++ 3D-Printing/ceramic-printer/firmware/Marlin-Configurations/Configuration.h
@@ -96,7 +96,7 @@ Here are some standard links for getting your machine calibrated:
 // 2 = X-Box 360 203Watts (the blue wire connected to PS_ON and the red wire to VCC)
 // :{1:'ATX',2:'X-Box 360'}
 
-#define POWER_SUPPLY 1
+#define POWER_SUPPLY 2
 
 // Define this to have the electronics keep the power supply off on startup. If you don't know what this is leave it.
 //#define PS_DEFAULT_OFF
