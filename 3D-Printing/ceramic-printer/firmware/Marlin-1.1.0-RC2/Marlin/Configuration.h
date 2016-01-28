483e7bcebd2e7eddb0dc9b92c2a59ee257e3a50c
Lars Busack <post@larsbusack.de>
2016-01-28 16:12:28 +0100

    Fixing Radius

--- 3D-Printing/ceramic-printer/firmware/Marlin-1.1.0-RC2/Marlin/Configuration.h
+++ 3D-Printing/ceramic-printer/firmware/Marlin-1.1.0-RC2/Marlin/Configuration.h
@@ -343,7 +343,7 @@ Here are some standard links for getting your machine calibrated:
   #define DELTA_RADIUS (DELTA_SMOOTH_ROD_OFFSET-DELTA_EFFECTOR_OFFSET-DELTA_CARRIAGE_OFFSET)
 
   // Print surface diameter/2 minus unreachable space (avoid collisions with vertical towers).
-  #define DELTA_PRINTABLE_RADIUS 140
+  #define DELTA_PRINTABLE_RADIUS 90
 
 #endif
 
@@ -431,12 +431,12 @@ const bool Z_MIN_PROBE_ENDSTOP_INVERTING = false; // set to true to invert the l
 // @section machine
 
 // Travel limits after homing (units are in mm)
-#define X_MIN_POS -DELTA_PRINTABLE_RADIUS
-#define Y_MIN_POS -DELTA_PRINTABLE_RADIUS
-#define Z_MIN_POS 0
 #define X_MAX_POS DELTA_PRINTABLE_RADIUS
+#define X_MIN_POS -DELTA_PRINTABLE_RADIUS
 #define Y_MAX_POS DELTA_PRINTABLE_RADIUS
+#define Y_MIN_POS -DELTA_PRINTABLE_RADIUS
 #define Z_MAX_POS MANUAL_Z_HOME_POS
+#define Z_MIN_POS 0
 
 //===========================================================================
 //========================= Filament Runout Sensor ==========================
